37fe57c3261eb0932eecdb47e6b59a17d42a15b7
checho16 <sergioardila1996@gmail.com>
2024-09-06 09:23:38 -0500

    primer comm2

--- Cliente/app.py
+++ Cliente/app.py
@@ -1,19 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
+# Datos de ejemplo de usuarios
+users = {'empleado@atlas.com': 'password123', 'admin': '12345'}
 @app.route('/')
 def index():
     return render_template('login.html')
 
 @app.route('/login', methods=['POST'])
 def login():
-    # Aquí puedes añadir la lógica para autenticar al usuario
-    # y generar el código de respuesta
+    username = request.form['username']
+    password = request.form['password']
+  
+    if username in users and users[username] == password:
+        # return 'Inicio de sesión exitoso'
+        # codigo = '12345'  # Código de ejemplo
+        codigo = username 
+        # return render_template('codigo.html', codigo=codigo)
+        return render_template('codigo.html', codigo=codigo)
+    else:
+        return '<script>alert("Credenciales incorrectas");</script>'+ render_template('login.html')
 
-    codigo = '12345'  # Código de ejemplo
-
-    return render_template('codigo.html', codigo=codigo)
+        
 
 if __name__ == '__main__':
     app.run(debug=True)
