96b2f3c9b1c0c6245a7884eae328d87f1554d4b1
checho16 <sergioardila1996@gmail.com>
2024-09-06 16:02:34 -0500

    primer comm3

--- Cliente/app.py
+++ Cliente/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request
-
+import datetime
 app = Flask(__name__)
 # Datos de ejemplo de usuarios
 users = {'empleado@atlas.com': 'password123', 'admin': '12345'}
+
+
 @app.route('/')
 def index():
     return render_template('login.html')
@@ -15,13 +17,17 @@ def login():
     if username in users and users[username] == password:
         # return 'Inicio de sesión exitoso'
         # codigo = '12345'  # Código de ejemplo
-        codigo = username 
+        codigo = username
+        fecha= datetime.datetime.now()
+        servidor='http://127.0.0.1:5001/personas'
+        if username=='empleado@atlas.com':
+            name='John Doe'
+        else:
+            name=codigo
         # return render_template('codigo.html', codigo=codigo)
-        return render_template('codigo.html', codigo=codigo)
+        return render_template('codigo.html', codigo=codigo,fecha=fecha,servidor=servidor,name=name)
     else:
         return '<script>alert("Credenciales incorrectas");</script>'+ render_template('login.html')
 
-        
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)       
